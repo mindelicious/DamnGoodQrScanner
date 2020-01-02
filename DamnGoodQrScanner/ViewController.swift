333c58d688360ba6438d115bd0e2b09ed5e6dd6d
Mateusz Danieluk <mateusz.danieluk@skyrise.tech>
2020-01-02 14:29:35 +0100

    Initial Commit

---
+++ DamnGoodQrScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DamnGoodQrScanner
+//
+//  Created by Mateusz Danieluk on 02/01/2020.
+//  Copyright © 2020 Mateusz Danieluk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
