cbacece1c4760d51636844734fd8afe0d8bee18e
Mateusz Danieluk <mateusz.danieluk@skyrise.tech>
2020-01-07 13:13:52 +0100

    🐢deleting swiftQRScanner library

---
+++ DamnGoodQrScanner/Model/ScanModelProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  ScanModelProtocol.swift
+//  DamnGoodQrScanner
+//
+//  Created by Mateusz Danieluk on 07/01/2020.
+//  Copyright © 2020 Mateusz Danieluk. All rights reserved.
+//
+
+import Foundation
